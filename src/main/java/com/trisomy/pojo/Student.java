b8afb8dfacc4e4d195f4895386b8b42bf0ded88d
HaoH <3492689458@qq.com>
2022-11-01 18:59:08 +0800

    hot-fix commit2

--- src/main/java/com/trisomy/pojo/Student.java
+++ src/main/java/com/trisomy/pojo/Student.java
@@ -3,14 +3,18 @@ package com.trisomy.pojo;
 /**
  * @author 三体
  * @version 1.0
- * @className Student
  * @since 1.0
  **/
 public class Student {
     public static void main(String[] args) {
         System.out.println("hello git！！");
-        System.out.println("洛洛历险记");
-        System.out.println("frfnrriofe");
-        System.out.println("hhhhhhhhhhhhhhhhhhhhhhhhhhh");
+        System.out.println("hello git！！2");
+        System.out.println("hello git！！3");
+        System.out.println("hello git！！4");
+        System.out.println("hello git！！5");
+        System.out.println("hello git！！6");
+        System.out.println("hot-fix ！！！！！commit1");
+
+
     }
 }
