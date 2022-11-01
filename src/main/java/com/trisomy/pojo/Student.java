889ac550445fefdc8a49c6d139dfaa6166fa72a2
HaoH <3492689458@qq.com>
2022-11-01 16:29:24 +0800

    变更

---
+++ src/main/java/com/trisomy/pojo/Student.java
@@ -0,0 +1,13 @@
+package com.trisomy.pojo;
+
+/**
+ * @author ÈýÌå
+ * @version 1.0
+ * @className Student
+ * @since 1.0
+ **/
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("hello git£¡£¡");
+    }
+}
